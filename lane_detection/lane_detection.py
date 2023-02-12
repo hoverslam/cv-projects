51411e8d6e021e1795f5a47a9def81ca66786bc6
hoverslam <hofmann.christopher@outlook.com>
2023-02-12 22:14:59 +0100

    Only display lines with absolute slope >20

--- lane_detection/lane_detection.py
+++ lane_detection/lane_detection.py
@@ -2,10 +2,16 @@ import numpy as np
 import cv2 as cv
 
 
-# TODO: Remove horizontal lines
 # TODO: Merge nearby lines
 # TODO: Only display lines that belong to right lane
 # TODO: Display the lane more beautiful, e.g. area between lines
+
+
+def calculate_slope(line: np.ndarray) -> float:
+    x1, y1, x2, y2 = line.squeeze()
+    slope = np.arctan2(y2 - y1, x2 - x1)
+
+    return np.degrees(slope)
 
 
 def find_lines(frame: np.ndarray) -> np.ndarray:
@@ -19,24 +25,28 @@ def find_lines(frame: np.ndarray) -> np.ndarray:
     cv.fillPoly(mask, roi, 255)
     masked_frame = cv.bitwise_and(canny, mask)
 
-    # Detect lines
+    # Detect lines (Probabilistic Hough Transform works better)
     lines = cv.HoughLinesP(masked_frame, 2, np.pi/180, 100, np.array([]),
-                           minLineLength=50, maxLineGap=100)
+                           minLineLength=50, maxLineGap=200)
 
     return lines
 
 
+def show_lines(frame: np.ndarray, lines: np.ndarray) -> None:
+    if lines is not None:
+        for line in lines:
+            if abs(calculate_slope(line)) > 20:
+                x1, y1, x2, y2 = line.squeeze()
+                cv.line(frame, (x1, y1), (x2, y2), (0, 0, 255))
+
+
 if __name__ == "__main__":
-    cap = cv.VideoCapture("videos/test_hard.mp4")
+    cap = cv.VideoCapture("videos/test_simple.mp4")
 
     while cap.isOpened():
         _, frame = cap.read()
         lines = find_lines(frame)
-        if lines is not None:
-            for line in lines:
-                x1, y1, x2, y2 = line.squeeze()
-                cv.line(frame, (x1, y1), (x2, y2), (0, 0, 255))
-
+        show_lines(frame, lines)
         cv.imshow("Dash Cam", frame)
         if cv.waitKey(1) == ord("q"):
             break
